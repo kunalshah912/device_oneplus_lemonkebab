0f6bb80cff2fbf51f9ec54dcb604ddc390df54e9
kunalshah <kunalshah912@gmail.com>
2021-10-30 16:49:59 +0530

    switch audio effects to 6.0 from 7.0
    *fixes audio issue in third party apps
    
    Signed-off-by: kunalshah <kunalshah912@gmail.com>

--- device.mk
+++ device.mk
@@ -135,7 +135,7 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@7.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.soundtrigger@2.3-impl \
